7b0824b0f003f57144cec557bd9c75dcaf97e1da
dbvargas <dbv22@cornell.edu>
2024-10-22 18:23:18 -0400

    added codebert embedding gen

---
+++ gen_codebert_embeddings.py
@@ -0,0 +1,80 @@
+import argparse
+import os
+
+import torch
+from transformers import AutoModel, AutoTokenizer
+
+CHECKPOINTS = {
+    "codet5": "Salesforce/codet5p-110m-embedding",
+    "codellama": "meta-llama/CodeLlama-7b-hf",
+    "codebert": "microsoft/codebert-base",
+}
+DATASET_PATH = "data/stack/estimationset"
+EMBEDDING_PATH = "embeddings"
+LANGUAGES = ["java", "javascript", "php", "python"]
+
+
+def load_model_and_tokenizer(model_type: str, device: str):
+    checkpoint = CHECKPOINTS[model_type]
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
+    model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
+    return model, tokenizer
+
+
+def create_dirs(model_type: str) -> None:
+    for lang in LANGUAGES:
+        os.makedirs(os.path.join(EMBEDDING_PATH, model_type, lang), exist_ok=True)
+
+
+def stream_snippets(lang: str):
+    lang_path: str = os.path.join(DATASET_PATH, lang)
+    snippet_paths = map(lambda p: os.path.join(lang, p), os.listdir(lang_path))
+
+    for snippet_path in snippet_paths:
+        with open(os.path.join(DATASET_PATH, snippet_path), "r", encoding="utf-8") as f:
+            yield snippet_path, f.read()
+
+
+def gen_embedding(source: str, model, tokenizer, device: str):
+    inputs = tokenizer.encode(source, return_tensors="pt", truncation=True, max_length=512).to(device)
+    with torch.no_grad():
+        return model(inputs)[0]
+
+
+def save_embedding(snippet_path: str, embedding, model_type: str):
+    embedding_path = os.path.join(EMBEDDING_PATH, model_type, snippet_path + ".pt")
+    torch.save(embedding, embedding_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate embeddings using CodeT5 or CodeLlama"
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        required=True,
+        choices=["codet5", "codellama", "codebert"],
+        help="The model to use for generating embeddings: codet5 or codellama.",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cpu",
+        choices=["cpu", "cuda"],
+        help="Device to use for inference: cpu or cuda (GPU)",
+    )
+
+    args = parser.parse_args()
+    model_type = args.model
+    device = args.device
+
+    model, tokenizer = load_model_and_tokenizer(model_type, device)
+
+    create_dirs(model_type)
+
+    # Generate and save embeddings for each language
+    for lang in LANGUAGES:
+        for snippet_path, snippet in stream_snippets(lang):
+            embedding = gen_embedding(snippet, model, tokenizer, device)
+            save_embedding(snippet_path, embedding, model_type)
